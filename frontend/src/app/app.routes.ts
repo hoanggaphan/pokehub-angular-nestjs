2c7e03b626f80377b493e324efa91eda6b0e44b5
Phan Hoang <dev.phansihoang@gmail.com>
2025-08-13 13:11:26 +0700

    🎨 chore: Remove unused CSS file and update app structure with routing for Home, Pokemon List, and Not Found pages.

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { NotFound } from './pages/not-found/not-found';
+import { PokeList } from './pages/poke-list/poke-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home,
+        title: 'Home Page'
+    },
+    {
+        path: 'pokemon-list',
+        component: PokeList,
+        title: 'Pokemon List Page'
+    },
+    { path: '**', component: NotFound }
+];
---
+++ frontend/src/app/pages/poke-list/poke-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-poke-list',
+  imports: [],
+  templateUrl: './poke-list.html',
+  styleUrl: './poke-list.css'
+})
+export class PokeList {
+
+}
