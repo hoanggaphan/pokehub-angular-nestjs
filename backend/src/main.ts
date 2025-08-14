bb0ef8f79003bf5def144aee37321c8bacad6724
Phan Hoang <dev.phansihoang@gmail.com>
2025-08-14 11:14:08 +0700

    ✨ feat: Enhance application bootstrapping by enabling CORS based on environment, implementing global validation pipes for request data, and updating the listening port configuration.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// import { json, urlencoded } from 'express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'development' ? '*' : process.env.CLIENT,
+  });
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  // app.use(json({ limit: '50mb' }));
+  // app.use(urlencoded({ extended: true, limit: '50mb' }));
+  await app.listen(process.env.APP_PORT ?? 3000);
 }
-bootstrap();
+
+void bootstrap();
